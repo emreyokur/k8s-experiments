2cec96a4b65249b6df208ec4e2e6932b99a00a3f
Emre Yüksel Okur <emreyokur@gmail.com>
2021-10-24 21:25:52 +0300

    Removed encryption

--- install-consul-agent.sh
+++ install-consul-agent.sh
@@ -6,14 +6,13 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install consul
 
-export ENCRYPT=$(consul keygen)
+#export ENCRYPT=$(consul keygen)
 
 mkdir --parents /etc/consul.d
 
 cat > consul.hcl << EOF
 datacenter = "dc1"
 data_dir = "/opt/consul"
-encrypt = "$ENCRYPT"
 ca_file = "/etc/consul.d/consul-agent-ca.pem"
 cert_file = "/etc/consul.d/dc1-client-consul-0.pem"
 key_file = "/etc/consul.d/dc1-client-consul-0-key.pem"
