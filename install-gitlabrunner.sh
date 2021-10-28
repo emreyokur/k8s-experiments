0df1a557299869653d1c175a4e3f5ad04dc38c25
Emre Yüksel Okur <emreyokur@gmail.com>
2021-10-28 23:03:17 +0300

    Gitlab Runner

---
+++ install-gitlabrunner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+set -x
+
+# Download the binary for your system
+curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+# Give it permissions to execute
+chmod +x /usr/local/bin/gitlab-runner
+
+# Create a GitLab CI user
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+
+# Install and run as service
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+gitlab-runner start
