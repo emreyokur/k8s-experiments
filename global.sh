0a5c6663840dd7d7ff114cff1fd633d0fa77c49f
Emre Okur <emreyokur@gmail.com>
2021-12-05 21:47:11 +0300

    Update global.sh

--- global.sh
+++ global.sh
@@ -8,3 +8,8 @@ export WORK_DIR=/home
 export CLUSTER_NAME="case-emre-yuksel-okur"
 export CLUSTER_DOMAIN="abc"
 export CLUSTER_FQDN="$CLUSTER_NAME.$CLUSTER_DOMAIN"
+
+ssh-keygen -t rsa -N '' -f $ROOT_DIR/.ssh/id_rsa <<< y
+ssh-copy-id -i $ROOT_DIR/.ssh/id_rsa.pub $SERVER_1
+ssh-copy-id -i $ROOT_DIR/.ssh/id_rsa.pub $SERVER_2
+ssh-copy-id -i $ROOT_DIR/.ssh/id_rsa.pub $SERVER_3
